fc1570dd78f3e3a9b523fca6ef7c17cb74aceea7
jadsaidd <jadsaidd@gmail.com>
2022-10-18 13:12:43 +0300

    Pull this

--- lib/Auctions/ui/widget/auctions_card.dart
+++ lib/Auctions/ui/widget/auctions_card.dart
@@ -7,136 +7,66 @@ import 'package:money_converter/money_converter.dart';
 import '../../../AuctionsWatchInfo/ui/screen/AuctionWatchInfo.dart';
 import '../../../utils/Images/Images.dart';
 
-class AuctionsCard extends StatefulWidget {
+class AuctionsCard extends StatelessWidget {
   final AuctionsModel auctionsModel;
   int index;
   AuctionsCard({Key? key, required this.auctionsModel,required this.index}) : super(key: key);
 
-  @override
-  State<AuctionsCard> createState() => _AuctionsCardState();
-}
-
-class _AuctionsCardState extends State<AuctionsCard> {
-  String? AEDtoUSD;
-
-  @override
-  void initState() {
-    super.initState();
-    getAmounts();
-  }
-
-// call function to convert
-  void getAmounts() async {
-    var usdConvert = await MoneyConverter.convert(
-        Currency(Currency.AED, amount: widget.auctionsModel.price),
-        Currency(Currency.USD));
-    setState(() {
-      AEDtoUSD = usdConvert.toString();
-    });
-  }
+  const AuctionsCard({Key? key, required this.auctionsModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.38,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => AuctionsInfo(
-                    auctionsModel: widget.auctionsModel,
-                    index: widget.index,
-                  )));
-        },
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.38,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Stack(
-              children:[Padding(
-                padding:
-                const EdgeInsets.only(left: 11, right: 11, top: 13, bottom: 11),
-                child:
-                Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0))),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 10, top: 9, right: 10),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Center(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height * 0.16,
-                                  width: double.infinity,
-                                  child: Image.asset(
-                                    "${widget.auctionsModel.image}",
-                                    fit: BoxFit.contain,
-                                  ),
-                                ),
-                                SizedBox(height: 20),
-                                Text(
-                                  "${widget.auctionsModel.text}",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFF000000)),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                  softWrap: true,
-                                ),
-                                SizedBox(height: 10),
-                                Row(
-                                  children: [
-                                    Text(widget.auctionsModel.status == true
-                                        ? "Selling Price ${Currency.AED}${widget.auctionsModel.price} = $AEDtoUSD ${Currency.USD}"
-                                    : widget.auctionsModel.status == false
-                                ? "Last Bid ${Currency.AED}${widget.auctionsModel.price} = $AEDtoUSD ${Currency.USD}"
-                                    : "Asking Price ${Currency.AED}${widget.auctionsModel.price} = $AEDtoUSD ${Currency.USD}",                                        style: TextStyle(
-                                            fontSize: 15,
-                                            color: Color(0xFF777777))),
-                                  ],
-                                ),
-                                SizedBox(height: 10),
-                                Text("${widget.auctionsModel.date}",
-                                    style: TextStyle(
-                                        fontSize: 10, color: Color(0xFF777777))),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AuctionsInfo(auctionsModel: auctionsModel, index: 1,)));
+      },
+      child: SizedBox(
+        height: MediaQuery
+            .of(context)
+            .size
+            .height * 0.35,
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 11, right: 11, top: 13, bottom: 11),
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 32.0, top: 9, right: 23),
+              child: Column(
+                children: [
+                  Text("${auctionsModel.text}", style: TextStyle(fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000)),),
+                  Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: MediaQuery
+                              .of(context)
+                              .size
+                              .height * 0.16,
+                          width: double.infinity,
+                          child: Image.asset("${auctionsModel.image}",
+                            fit: BoxFit.contain,),
+                        ),
+                        Text("${auctionsModel.status}", style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFD4181E))),
+                        Text("Last Bid AED${auctionsModel
+                            .price1} = ${auctionsModel.price2}",
+                            style: TextStyle(fontSize: 15,
+                                color: Color(0xFF777777))),
+                      ],
                     ),
                   ),
-                ),),
-                widget.auctionsModel.status == true?
-                Positioned.fill(
-                    child: Align(
-                        alignment: Alignment.topRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 15.0),
-                          child: Container(
-                              height: MediaQuery.of(context).size.height*0.1,
-                              width: MediaQuery.of(context).size.width*0.1,
-                              child: Image.asset(ImageAsset.SOLDOUT)),
-                        )
-                      // Text("${auctionsModel.status}",
-                      //     style: TextStyle(
-                      //         fontSize: 15,
-                      //         fontWeight: FontWeight.bold,
-                      //         color: Color(0xFFD4181E))),
-                    )):Container()
-              ]),
+
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
